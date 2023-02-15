904789c42a26691ab5243365ee72555ee6b85b47
Vinícius Machado <105569198+vinnyedu@users.noreply.github.com>
2023-02-15 19:37:32 -0300

    aula 03

---
+++ aula-03.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/////////////////////////////////////////////////////////////////////
+// Booleanos
+
+const verdadeiro = true;
+const falso = false;
+
+const comparacao= 10 < 5
+console.log(comparacao, typeof comparacao);
+
+// Operadores
+// maior >
+// menor <
+
+// Igualdade
+// se é igual a ==
+// (outra forma) se é igual a ===
+
+console.log(`'10' == 10`, '10' == 10);        // == compara os valores, independente do tipo
+console.log(`'10' === 10`, '10' === 10);       // === compara os valores e os tipos
+
+// Diferença
+// se é diferente de !=
+// (outra forma mais assertiva) se é diferente de !==
+
+console.log(`'10' != 10`, '10' != 10);        // != compara os valores, independente do tipo
+console.log(`'10' !== 10`, '10' !== 10);       // !== compara os valores e os tipos
+
+/////////////////////////////////////////////////////////////////////
