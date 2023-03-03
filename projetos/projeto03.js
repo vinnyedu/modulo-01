ddae5d548a915a83b0606d6ffad8df0775b7b47f
Vinícius Machado <105569198+vinnyedu@users.noreply.github.com>
2023-03-03 20:05:37 -0300

    projeto 3 v2

--- projetos/projeto03.js
+++ projetos/projeto03.js
@@ -9,16 +9,113 @@ console.clear();
 
 // Criação da Matriz
 
-function matriz(linhas = 3, colunas = 3) {
-  const matriz = [];
-  for (let linha = 0; linha < linhas; linha++) {
-    const element = [];
-    for (let coluna = 0; coluna < colunas; coluna++) {
-      element.push(' ');
-    }
-    matriz.push(element);
-  }
-  return matriz;
-}
+const matriz = [
+  [0, 0, 0],
+  [0, 0, 0],
+  [0, 0, 0],
+];
 
-// Solicitar coordenadas aos jogadores
+// for (let linha = 0; linha < 3; linha++) {
+//   const element = [];
+//   for (let coluna = 0; coluna < 3; coluna++) {
+//     element.push(' ');
+//   }
+//   matriz.push(element);
+// }
+
+const jogoVelha = matriz;
+
+let vitoria = false;
+let vencedor;
+
+// Enquanto o jogo da velha estiver rodando
+
+while (true) {
+  // Atualização de jogadas
+
+  const linhaDesejada = +prompt('Digite a linha: ');
+  const colunaDesejada = +prompt('Digite a coluna: ');
+
+  if (jogoVelha[linhaDesejada][colunaDesejada]) {
+    console.log('Espaço opudado! Digite outra...');
+
+    continue;
+  }
+
+  const jogada = prompt('Qual é a jogada (X ou O): ');
+
+  jogoVelha[linhaDesejada][colunaDesejada] = jogada;
+
+  // Chegagem de coordenadas vazias (verifica se o jogo acabou)
+
+  let coordenadasVazias = 0;
+
+  for (let linhaAtual = 0; linhaAtual < jogoVelha.length; linhaAtual++) {
+    const linha = jogoVelha[linhaAtual];
+
+    for (let coluna = 0; coluna < linha.length; coluna++) {
+      const jogadaAtual = linha[coluna];
+
+      if (!jogadaAtual) {
+        coordenadasVazias++;
+      }
+    }
+  }
+  if (coordenadasVazias === 0) {
+    break;
+  }
+
+  // Condições de vitoria
+
+  const condicoesVitoria = [
+    // Colunas
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+
+    // Linhas
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+
+    // Diagonais
+    [1, 5, 9],
+    [3, 5, 7],
+  ];
+
+  for (const condicaoVitoria of condicoesVitoria) {
+    // console.log(`Checagem da condição de vitória: ${condicaoVitoria}`);
+
+    let quantidadeValoresIguais = 0;
+    let ultimoValor;
+
+    for (const coordenada of condicaoVitoria) {
+      const linha = parseInt((coordenada - 1) / 3);
+      const coluna = (coordenada - 1) % 3;
+      const jogada = matriz[linha][coluna];
+
+      // console.log({ jogada });
+
+      if (jogada && (!ultimoValor || jogada === ultimoValor)) {
+        quantidadeValoresIguais++;
+      }
+      ultimoValor = jogada;
+    }
+    if (quantidadeValoresIguais === 3) {
+      vitoria = true;
+      vencedor = ultimoValor;
+
+      break;
+    }
+    // console.log({ quantidadeValoresIguais });
+
+    // console.log();
+  }
+}
+// Resultados finais
+
+if (vitoria) {
+  console.log(`O jogador ${vencedor} venceu a partida`);
+} else {
+  console.log('A partida terminou em empate');
+}
