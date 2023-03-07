e4a39de4b035f3e0ccbad4f25616a96353fab382
Vinícius Machado <105569198+vinnyedu@users.noreply.github.com>
2023-03-07 19:27:16 -0300

    projeto 3 v3

--- projetos/projeto03.js
+++ projetos/projeto03.js
@@ -15,107 +15,136 @@ const matriz = [
   [0, 0, 0],
 ];
 
-// for (let linha = 0; linha < 3; linha++) {
-//   const element = [];
-//   for (let coluna = 0; coluna < 3; coluna++) {
-//     element.push(' ');
-//   }
-//   matriz.push(element);
-// }
+const matrizExemplo = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
 
-const jogoVelha = matriz;
+let jogoVelha = matriz;
 
 let vitoria = false;
 let vencedor;
+let jogada = 'X';
+let jogadorX = 0,
+  jogadorO = 0;
 
 // Enquanto o jogo da velha estiver rodando
-
 while (true) {
-  // Atualização de jogadas
+  while (!vitoria) {
+    // Atualização de jogadas
+    for (let i = 0; i < 3; i++) {
+      console.log(matrizExemplo[i], jogoVelha[i]);
+    }
 
-  const linhaDesejada = +prompt('Digite a linha: ');
-  const colunaDesejada = +prompt('Digite a coluna: ');
+    console.log(`É a vez do jogador ${jogada}`);
 
-  if (jogoVelha[linhaDesejada][colunaDesejada]) {
-    console.log('Espaço opudado! Digite outra...');
+    const coordenadaDesejada = +prompt('Digite a coordenada desejada: ');
+    const y = parseInt((coordenadaDesejada - 1) / 3);
+    const x = (coordenadaDesejada - 1) % 3;
 
-    continue;
-  }
+    if (jogoVelha[y][x]) {
+      console.log('Espaço opudado! Digite outra...');
 
-  const jogada = prompt('Qual é a jogada (X ou O): ');
+      continue;
+    }
 
-  jogoVelha[linhaDesejada][colunaDesejada] = jogada;
+    jogoVelha[y][x] = jogada;
 
-  // Chegagem de coordenadas vazias (verifica se o jogo acabou)
+    // Inverter o jogador
+    jogada = jogada === 'X' ? 'O' : 'X';
 
-  let coordenadasVazias = 0;
+    // Chegagem de coordenadas vazias (verifica se o jogo acabou)
 
-  for (let linhaAtual = 0; linhaAtual < jogoVelha.length; linhaAtual++) {
-    const linha = jogoVelha[linhaAtual];
+    let coordenadasVazias = 0;
 
-    for (let coluna = 0; coluna < linha.length; coluna++) {
-      const jogadaAtual = linha[coluna];
+    for (let linhaAtual = 0; linhaAtual < jogoVelha.length; linhaAtual++) {
+      const linha = jogoVelha[linhaAtual];
 
-      if (!jogadaAtual) {
-        coordenadasVazias++;
+      for (let coluna = 0; coluna < linha.length; coluna++) {
+        const jogadaAtual = linha[coluna];
+
+        if (!jogadaAtual) {
+          coordenadasVazias++;
+        }
       }
     }
-  }
-  if (coordenadasVazias === 0) {
-    break;
-  }
-
-  // Condições de vitoria
-
-  const condicoesVitoria = [
-    // Colunas
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-
-    // Linhas
-    [1, 4, 7],
-    [2, 5, 8],
-    [3, 6, 9],
-
-    // Diagonais
-    [1, 5, 9],
-    [3, 5, 7],
-  ];
-
-  for (const condicaoVitoria of condicoesVitoria) {
-    // console.log(`Checagem da condição de vitória: ${condicaoVitoria}`);
-
-    let quantidadeValoresIguais = 0;
-    let ultimoValor;
-
-    for (const coordenada of condicaoVitoria) {
-      const linha = parseInt((coordenada - 1) / 3);
-      const coluna = (coordenada - 1) % 3;
-      const jogada = matriz[linha][coluna];
-
-      // console.log({ jogada });
-
-      if (jogada && (!ultimoValor || jogada === ultimoValor)) {
-        quantidadeValoresIguais++;
-      }
-      ultimoValor = jogada;
-    }
-    if (quantidadeValoresIguais === 3) {
-      vitoria = true;
-      vencedor = ultimoValor;
-
+    if (coordenadasVazias === 0) {
       break;
     }
-    // console.log({ quantidadeValoresIguais });
 
-    // console.log();
+    // Condições de vitoria
+
+    const condicoesVitoria = [
+      // Colunas
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9],
+
+      // Linhas
+      [1, 4, 7],
+      [2, 5, 8],
+      [3, 6, 9],
+
+      // Diagonais
+      [1, 5, 9],
+      [3, 5, 7],
+    ];
+
+    for (const condicaoVitoria of condicoesVitoria) {
+      // console.log(`Checagem da condição de vitória: ${condicaoVitoria}`);
+
+      let quantidadeValoresIguais = 0;
+      let ultimoValor;
+
+      for (const coordenada of condicaoVitoria) {
+        const linha = parseInt((coordenada - 1) / 3);
+        const coluna = (coordenada - 1) % 3;
+
+        const jogada = matriz[linha][coluna];
+
+        // console.log({ jogada });
+
+        if (jogada && (!ultimoValor || jogada === ultimoValor)) {
+          quantidadeValoresIguais++;
+        }
+        ultimoValor = jogada;
+      }
+      if (quantidadeValoresIguais === 3) {
+        vitoria = true;
+        vencedor = ultimoValor;
+
+        break;
+      }
+      // console.log({ quantidadeValoresIguais });
+
+      // console.log();
+    }
   }
-}
-// Resultados finais
+  // Resultados finais
 
-if (vitoria) {
-  console.log(`O jogador ${vencedor} venceu a partida`);
-} else {
-  console.log('A partida terminou em empate');
+  if (vitoria) {
+    console.log(`O jogador ${vencedor} venceu a partida`);
+    if (vencedor === 'X') {
+      jogadorX++;
+    } else {
+      jogadorO++;
+    }
+  } else {
+    console.log('A partida terminou em empate');
+  }
+  console.log(`Jogador X - ${jogadorX} x ${jogadorO} - Jogador O`);
+
+  const continuar = prompt('Deseja continuar?[s/n]');
+  if (continuar === 'n') {
+    break;
+  } else {
+    vitoria = false;
+    jogada = 'X';
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        jogoVelha[i][j] = 0;
+      }
+    }
+  }
 }
