99a0d7b3520ae3115d42c47d42e6866d77746af5
Vinícius Machado <105569198+vinnyedu@users.noreply.github.com>
2023-03-09 19:02:17 -0300

    add verificarDados

--- projetos/projeto-JogoDados.js
+++ projetos/projeto-JogoDados.js
@@ -10,9 +10,56 @@
    - Mostrar no final qual jogador ganhou mais rodadas e foi o grande campeão.
 */
 
+const prompt = require('prompt-sync')({ sigint: true });
+console.clear();
+
 class jogadas {
   constructor(nome, jogada) {
     this.nome = nome;
     this.jogada = jogada;
+    this.vitorias = 0
   }
 }
+
+function verificarDados(params = 'Nome') {
+  while (true) {
+    let dado;
+
+    if (params === 'Nome') {
+      dado = prompt('Nome do jogador: ');
+      dado = dado.trim().toLowerCase();
+      if (dado.length < 3) {
+        console.log(`${params} inválido. deve ser maior que 3.`);
+        continue;
+      }
+      dado = dado.charAt(0).toUpperCase() + dado.slice(1);
+    } else {
+      dado = +prompt(`Número de ${params}: `);
+      if (dado < 1 || isNaN(dado)) {
+        console.log('Número de deve ser maior que zero');
+        continue;
+      }
+    }
+    return dado;
+  }
+}
+
+function main() {
+  const jogadores = [];
+
+  const numRodadas = verificarDados('Rodadas');
+  const numJogadores = verificarDados('Jogadores');
+
+  // Nome dos Jogadores
+  for (let i = 0; i < numJogadores; i++) {
+    const jogador = verificarDados('Nome');
+    jogadores.push(jogador);
+  }
+
+  for (let i = 0; i < numRodadas; i++) {
+    const element = array[i];
+    
+  }
+}
+
+main();
